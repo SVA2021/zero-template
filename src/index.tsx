f20b5818eb4295f99b28900046cd88682caf5a34
SVA2021 <sofronov.vit@gmail.com>
2022-11-02 13:45:34 +0400

    upd initial files

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '../src/styles/global.scss';
-import App from './App';
+import {App} from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
